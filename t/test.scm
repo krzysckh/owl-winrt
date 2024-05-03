58d076aa360085aca5b137e4f62c89fdf53d1080
krzysckh <krzysckh@wp.pl>
2024-05-03 19:52:17 +0200

    stu ff

--- t/test.scm
+++ t/test.scm
@@ -2,6 +2,7 @@
  (owl toplevel)
  (owl lazy)
  (owl sys)
+ (prefix (owl sys) sys/)
  (lib))
 
 (define (OK pred a b)
@@ -9,17 +10,9 @@
       (print "OK")
       (error "NOT OK" a)))
 
-(define (test-networking)
-  (print "testing networking...")
-  (let ((con (open-connection (resolve-host "suckless.org") 80)))
-    (write-bytes con (string->list "GET /\nHost: suckless.org\n\n"))
-    (llen (lines con))))
-
 (λ (_)
-  ;; (OK eqv? (foo) 'bar)
-  ;; (OK eqv? (print "Hello, World!") #t)
-  ;; (OK eqv? (file? "Makefile") #t)
-  ;; (OK > (test-networking) 0)
-  (sleep 10)
+  (OK eqv? (foo) 'bar)
+  (OK eqv? (print "Hello, World!") #t)
+  (OK eqv? (file? "Makefile") #t)
 
   0)
